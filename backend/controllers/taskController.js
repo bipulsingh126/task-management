3e01f16fb6cc1f0eba025a990ba31c719b8a9d72
bipulsingh126 <bipulpratapsingh126@gmail.com>
2024-11-28 04:46:47 +0530

    finsh backend

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,30 +1,77 @@
 import { taskmodel } from "../model/task.js";
 import { usermodel } from "../model/user.js";
 
-const createTask = async (req, res) => {
+const addTask = async (req, res) => {
   try {
-    const { title, desc } = req.body;
-    const { id } = req.headers;
-
-    // Validate required fields
-    if (!title || !desc) {
-      return res.status(400).json({
-        success: false,
-        message: "Title and description are required"
-      });
+    const { title, body, email } = req.body;
+    const existingUser = await usermodel.findOne({ email });
+    if (existingUser) {
+      const task = new taskmodel({ title, body, user: existingUser });
+      await task.save();
+      existingUser.tasks.push(task);
+      existingUser.save();
+      res.status(200).json({ success: true, message: "Task added" });
     }
-
-    const newTask = new taskmodel({ title, desc });
-    const savedTask = await newTask.save();
-    await usermodel.findByIdAndUpdate(id, { $push: { tasks: savedTask._id } });
-
-    res.status(200).json({ success: true, message: "Task created" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
 
+//update
 
+const updateTask = async (req, res) => {
+  try {
+    const { title, body, email } = req.body;
+    const existingUser = await usermodel.findOne({ email });
+    if (existingUser) {
+      const task = await taskmodel.findByIdAndUpdate(req.params.id, {
+        title,
+        body,
+      });
+      task.save();
+      res.status(200).json({ success: true, message: "Task updated" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
 
-export { createTask };
+// delete
+const deleteTask = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const existingUser = await usermodel.findOneAndUpdate(
+      { email },
+      { $pull: { tasks: req.params.id } }
+    );
+    if (existingUser) {
+      const task = await taskmodel.findByIdAndDelete(req.params.id);
+      res.status(200).json({ success: true, message: "Task deleted" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
+
+// get all tasks
+
+const getAllTasks = async (req, res) => {
+  try {
+    const task = await taskmodel
+      .find({ user: req.params.id })
+      .sort({ createdAt: -1 });
+    if (task.length !== 0) {
+      res.status(200).json({ success: true, message: "Task found", task });
+    } else {
+      res.status(200).json({ success: true, message: "Task not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
+
+export { addTask, updateTask, deleteTask, getAllTasks };
--- backend/routes/task.js
+++ backend/routes/task.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import { createTask } from '../controllers/taskController.js';
-import authanticateToken from '../middleware/auth.js';
+import express from "express";
+import { addTask, deleteTask, getAllTasks, updateTask } from "../controllers/taskController.js";
 
 const taskRouter = express.Router();
 
-taskRouter.post('/create-task' , authanticateToken , createTask);
-
-
+taskRouter.post("/addtask", addTask);
+taskRouter.put("/updatetask/:id", updateTask);
+taskRouter.delete("/deletetask/:id" , deleteTask);
+taskRouter.get("/getalltasks/:id", getAllTasks);
 
 export default taskRouter;
