92e25fcbf2cf7280e9162426be1687fa2d9077e7
bipulsingh126 <bipulpratapsingh126@gmail.com>
2024-12-04 01:52:39 +0530

    fix some issue

--- client/src/component/Footer.jsx
+++ client/src/component/Footer.jsx
@@ -5,7 +5,7 @@ import { BiTask } from "react-icons/bi";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white">
+    <footer className="bg-gray-900 mt-5 text-white">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           {/* Brand Section */}
